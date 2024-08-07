d2535b35bddc4563771785c6e70af9de56a54945
YYuX-1145 <138500330+YYuX-1145@users.noreply.github.com>
2024-08-07 23:03:14 +0800

    Update GSV整合包安装依赖.bat

--- GSV整合包安装依赖.bat
+++ GSV整合包安装依赖.bat
@@ -1,2 +1,2 @@
-runtime\python.exe -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple gradio==3.38.0 gradio_client==0.8.1 colorlog 
+runtime\python.exe -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple gradio==3.50.2 colorlog 
 pause

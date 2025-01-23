717b9c4d9833f6135ecfcfbcda1cb86193dca463
YYuX-1145 <138500330+YYuX-1145@users.noreply.github.com>
2025-01-23 15:17:37 +0800

    update *

--- Sava_Utils/projects/bv2.py
+++ Sava_Utils/projects/bv2.py
@@ -51,7 +51,7 @@ class BV2(Projet):
             err=f'bert-vits2推理发生错误，请检查HiyoriUI是否正确运行。报错内容: {e}'
             logger.error(err)
             return None
-        
+
     def save_action(self,*args,text:str=None):
         language,port,mid,sid,speaker_name,sdp_ratio,noise_scale,noise_scale_w,length_scale,emotion_text=args
         sid,port,mid=positive_int(sid,port,mid)
@@ -60,3 +60,43 @@ class BV2(Projet):
         else:
             audio = self.api(text=text,mid=mid,spk_name=None,sid=sid,lang=language,length=length_scale,noise=noise_scale,noisew=noise_scale_w,sdp=sdp_ratio,split=False,style_text=None,style_weight=0,port=port,emotion=emotion_text)
         return audio
+
+    def UI(self):
+        with gr.Row():            
+            with gr.Column():
+                self.spkchoser=gr.Radio(label="选择说话人id或输入名称", choices=['输入id','输入名称'], value="输入id")
+                with gr.Row():
+                    self.model_id=gr.Number(label="模型id",value=0,visible=True,interactive=True)
+                    self.spkid=gr.Number(label="说话人ID",value=0,visible=True,interactive=True)
+                    self.speaker_name = gr.Textbox(label="说话人名称",visible=False,interactive=True)
+                self.language1 = gr.Dropdown(choices=['ZH','JP','EN','AUTO'], value='ZH', label="Language",interactive=True,allow_custom_value=False)
+                with gr.Accordion(label="参数",open=False):
+                    self.sdp_ratio = gr.Slider(minimum=0, maximum=1, value=0.2, step=0.1, label="SDP Ratio")
+                    self.noise_scale = gr.Slider(minimum=0.1, maximum=2, value=0.6, step=0.1, label="Noise Scale")
+                    self.noise_scale_w = gr.Slider(minimum=0.1, maximum=2, value=0.8, step=0.1, label="Noise Scale W")
+                    self.length_scale = gr.Slider(minimum=0.1, maximum=2, value=1, step=0.1, label="Length Scale")
+                    self.emo_text=gr.Textbox(label="text prompt",interactive=True,value="")
+                with gr.Row(): 
+                    self.sampling_rate1=gr.Number(label="采样率",value=44100,visible=True,interactive=True)                                
+                    self.api_port1=gr.Number(label="API Port",value=5000,visible=True,interactive=True)
+        self.gen_btn1 = gr.Button("生成", variant="primary", visible=True)
+        BV2_ARGS = [
+            self.sampling_rate1,
+            self.language1,
+            self.api_port1,
+            self.model_id,
+            self.spkid,
+            self.speaker_name,
+            self.sdp_ratio,
+            self.noise_scale,
+            self.noise_scale_w,
+            self.length_scale,
+            self.emo_text,
+        ]
+        return BV2_ARGS
+    
+    def arg_filter(self,*args):
+        in_file,fps,offset,max_workers,sr,language,port,mid,spkid,speaker_name,sdp_ratio,noise_scale,noise_scale_w,length_scale,emo_text=args
+        pargs=(language,port,mid,spkid,speaker_name,sdp_ratio,noise_scale,noise_scale_w,length_scale,emo_text)
+        kwargs={'in_file':in_file,'sr':sr,'fps':fps,'offset':offset,'proj':"bv2",'max_workers':max_workers}
+        return pargs,kwargs
--- Sava_Utils/projects/gsv.py
+++ Sava_Utils/projects/gsv.py
@@ -3,10 +3,47 @@ import requests
 import gradio as gr
 from ..utils import positive_int
 from .. import logger
+import os
+import hashlib
+import soundfile as sf
+
+current_path=os.environ.get("current_path")
+
+dict_language = {
+    "中文": "all_zh",
+    "粤语": "all_yue",
+    "英文": "en",
+    "日文": "all_ja",
+    "韩文": "all_ko",
+    "中英混合": "zh",
+    "粤英混合": "yue",
+    "日英混合": "ja",
+    "韩英混合": "ko",
+    "多语种混合": "auto",  # 多语种启动切分识别语种
+    "多语种混合(粤语)": "auto_yue",
+}
+cut_method = {
+    "不切": "cut0",
+    "凑四句一切": "cut1",
+    "凑50字一切": "cut2",
+    "按中文句号。切": "cut3",
+    "按英文句号.切": "cut4",
+    "按标点符号切": "cut5",
+}
+
+def temp_ra(a: tuple):
+    sr, wav = a
+    name = hashlib.md5(wav.tobytes()).hexdigest() + ".wav"
+    os.makedirs(os.path.join(current_path, "SAVAdata", "temp"), exist_ok=True)
+    dir = os.path.join(current_path, "SAVAdata", "temp", name)
+    if not os.path.exists(dir):
+        sf.write(dir, wav, sr)
+    return dir
 
 class GSV(Projet):
     def __init__(self):
         self.gsv_fallback=False
+        self.presets_list=['None']
         super().__init__("gsv")
 
     def api(self,port,**kwargs):
@@ -26,7 +63,7 @@ class GSV(Projet):
                 "speed": kwargs["speed_factor"],
                 }
                 API_URL = f"http://127.0.0.1:{port}/"
-            #print(data_json)
+            # print(data_json)
             response = requests.post(url=API_URL, json=data_json)
             response.raise_for_status()  # 检查响应的状态码
             return response.content
@@ -34,7 +71,7 @@ class GSV(Projet):
             err = f"GPT-SoVITS推理发生错误，请检查API服务是否正确运行。报错内容: {e}"
             logger.error(err)
             return None
-   
+
     def save_action(self, *args, text: str = None):
         text_language,port,refer_wav_path,aux_refer_wav_path,prompt_text,prompt_language,batch_size,batch_threshold,fragment_interval,speed_factor,top_k,top_p,temperature,repetition_penalty,split_bucket,text_split_method=args
         port = positive_int(port)[0]
@@ -62,3 +99,69 @@ class GSV(Projet):
             streaming_mode=False,
         )
         return audio
+
+    def UI(self):
+        self.language2 = gr.Dropdown(choices=dict_language.keys(), value="中英混合", label="Language",interactive=True,allow_custom_value=False)
+        with gr.Row():
+            self.refer_audio=gr.Audio(label="主参考音频")
+            self.aux_ref_audio = gr.File(label="辅参考音频(可选多个，或不选)",file_count="multiple")
+        with gr.Row():
+            self.refer_text=gr.Textbox(label="参考音频文本")
+            self.refer_lang = gr.Dropdown(choices=dict_language.keys(), value='中文', label="参考音频语言",interactive=True,allow_custom_value=False)
+        with gr.Accordion("模型切换",open=False):
+            self.sovits_path=gr.Textbox(value="",label="Sovits模型路径",interactive=True)
+            self.gpt_path=gr.Textbox(value="",label="GPT模型路径",interactive=True)
+            self.switch_gsvmodel_btn=gr.Button(value="切换模型",variant="primary")
+        with gr.Row():
+            self.sampling_rate2=gr.Number(label="采样率",value=32000,visible=True,interactive=True)
+            self.api_port2=gr.Number(label="API Port",value=9880,visible=True,interactive=True)
+        with gr.Accordion("高级合成参数",open=False):
+            self.batch_size = gr.Slider(minimum=1,maximum=200,step=1,label="batch_size",value=20,interactive=True)
+            self.batch_threshold = gr.Slider(minimum=0,maximum=1,step=0.01,label="batch_threshold",value=0.75,interactive=True)
+            self.fragment_interval = gr.Slider(minimum=0.01,maximum=1,step=0.01,label="分段间隔(秒)",value=0.3,interactive=True)
+            self.speed_factor = gr.Slider(minimum=0.25,maximum=4,step=0.05,label="speed_factor",value=1.0,interactive=True)
+            self.top_k = gr.Slider(minimum=1,maximum=100,step=1,label="top_k",value=5,interactive=True)
+            self.top_p = gr.Slider(minimum=0,maximum=1,step=0.05,label="top_p",value=1,interactive=True)
+            self.temperature = gr.Slider(minimum=0,maximum=1,step=0.05,label="temperature",value=1,interactive=True)
+            self.repetition_penalty = gr.Slider(minimum=0,maximum=2,step=0.05,label="repetition_penalty",value=1.35,interactive=True)
+            self.split_bucket = gr.Checkbox(label="数据分桶", value=True, interactive=True, show_label=True)
+            self.how_to_cut = gr.Radio(label="怎么切",choices=["不切","凑四句一切","凑50字一切","按中文句号。切","按英文句号.切","按标点符号切"],value="凑四句一切",interactive=True)
+        with gr.Accordion("预设", open=False):
+            self.choose_presets = gr.Dropdown(label="",value="None",choices=self.presets_list,interactive=True,allow_custom_value=True,)
+            self.desc_presets = gr.Textbox(label="", placeholder="描述信息，可选", interactive=True)
+            with gr.Row():
+                self.save_presets_btn = gr.Button(value="保存预设", variant="primary")
+                self.refresh_presets_btn = gr.Button(value="刷新", variant="secondary")
+        with gr.Row():
+            self.gen_btn2=gr.Button(value="生成",variant="primary",visible=True)
+        GSV_ARGS = [
+            self.sampling_rate2,
+            self.language2,
+            self.api_port2,
+            self.refer_audio,
+            self.aux_ref_audio,
+            self.refer_text,
+            self.refer_lang,
+            self.batch_size,
+            self.batch_threshold,
+            self.fragment_interval,
+            self.speed_factor,
+            self.top_k,
+            self.top_p,
+            self.temperature,
+            self.repetition_penalty,
+            self.split_bucket,
+            self.how_to_cut,
+        ]
+        return GSV_ARGS
+    
+    def arg_filter(self,*args):
+        in_file,fps,offset,max_workers,sr,language,port,refer_audio,aux_ref_audio,refer_text,refer_lang,batch_size,batch_threshold,fragment_interval,speed_factor,top_k,top_p,temperature,repetition_penalty,split_bucket,text_split_method=args
+        if refer_audio is None or refer_text == "":
+            gr.Warning("你必须指定参考音频和文本")
+            raise Exception("你必须指定参考音频和文本")
+        refer_audio_path=temp_ra(refer_audio)
+        aux_ref_audio_path=[i.name for i in aux_ref_audio] if aux_ref_audio is not None else []      
+        pargs=(dict_language[language],port,refer_audio_path,aux_ref_audio_path,refer_text,dict_language[refer_lang],batch_size,batch_threshold,fragment_interval,speed_factor,top_k,top_p,temperature,repetition_penalty,split_bucket,cut_method[text_split_method])
+        kwargs={'in_file':in_file,'sr':sr,'fps':fps,'offset':offset,'proj':"gsv",'max_workers':max_workers}
+        return pargs,kwargs

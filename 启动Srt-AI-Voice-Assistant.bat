ac7df4087d6c7e65996084dae3b608ee8dcefd6d
YYuX-1145 <138500330+YYuX-1145@users.noreply.github.com>
2025-03-01 15:49:39 +0800

    Update 启动Srt-AI-Voice-Assistant.bat

--- 启动Srt-AI-Voice-Assistant.bat
+++ 启动Srt-AI-Voice-Assistant.bat
@@ -1,3 +1,3 @@
 chcp 65001
-runtime\python.exe Srt-AI-Voice-Assistant.py -p 5001 ：：-p 指定端口 ，小于等于5001=自动 ，可选  -share：公网访问  -local 局域网访问
+runtime\python.exe Srt-AI-Voice-Assistant.py -p 0 ：：-p 指定端口并覆盖程序内设置，0=自动 ，可选  -share：公网访问  -local 局域网访问
 pause

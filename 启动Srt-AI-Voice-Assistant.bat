da39fafe30760fcaad812b4e7e2e19bffc2858a5
YYuX-1145 <138500330+YYuX-1145@users.noreply.github.com>
2025-03-22 11:38:04 +0800

    v4.1 fix

--- 启动Srt-AI-Voice-Assistant.bat
+++ 启动Srt-AI-Voice-Assistant.bat
@@ -1,4 +1,4 @@
 chcp 65001
 echo "启动脚本仅供参考；不会安装依赖请使用打包版"
-runtime\python.exe Srt-AI-Voice-Assistant.py -p 0 ：：-p 指定端口并覆盖程序内设置，0=自动 ，可选  -share：公网访问  -local 局域网访问
+runtime\python.exe Srt-AI-Voice-Assistant.py -p 0 ：：-p 指定端口并覆盖程序内设置，0=自动 ，可选  -server_mode：锁定大多数在多人环境下可能产生冲突的功能  -local 局域网访问
 pause

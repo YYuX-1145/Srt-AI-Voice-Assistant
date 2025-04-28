d37d2f5c05217cc601f4098ff34614fa41bebfb2
YYuX-1145 <138500330+YYuX-1145@users.noreply.github.com>
2025-04-28 22:06:16 +0800

    optimize custom api

--- Sava_Utils/tts_projects/custom.py
+++ Sava_Utils/tts_projects/custom.py
@@ -14,29 +14,29 @@ class Custom(TTSProjet):
         self.refresh_custom_api_list()
         super().__init__("custom", config)
 
-    def api(self, text):
-        return custom_api(text)
+    def api(self, func, text):
+        return func(text)
 
     def _UI(self):
         with gr.TabItem(i18n('Custom API')):
             with gr.Column():
-                gr.Markdown(value=MANUAL.getInfo("help_custom"))
-                self.choose_custom_api = gr.Dropdown(label=i18n('Choose Custom API Code File'), choices=self.custom_api_list, value=self.custom_api_list[0] if self.custom_api_list != [] else '', allow_custom_value=False)
-                self.refresh_custom_btn = gr.Button(value="🔄️")
-                self.gen_btn4 = gr.Button(value=i18n('Generate Audio'), variant="primary", visible=True)
+                gr.Markdown(value=MANUAL.getInfo("help_custom"))                
+                self.choose_custom_api = gr.Dropdown(label=i18n('Choose Custom API Code File'), choices=self.custom_api_list, value=self.custom_api_list[0] if self.custom_api_list != [] else '', allow_custom_value=False, scale=4)
+                with gr.Row():
+                    self.gen_btn4 = gr.Button(value=i18n('Generate Audio'), variant="primary", scale=8)
+                    self.refresh_custom_btn = gr.Button(value="🔄️", scale=1, min_width=40)                    
                 self.refresh_custom_btn.click(self.refresh_custom_api_list, outputs=[self.choose_custom_api])
         return []
 
-    def before_gen_action(self, *args, **kwargs):
+    def before_gen_action(self, custom_api_path, temp_namesp, **kwargs):
         # print(args)
-        custom_api_path = args[0]
         logger.info(f"Exec: custom_api_path {custom_api_path}")
         with open(os.path.join(current_path, "SAVAdata", "presets", custom_api_path), "r", encoding="utf-8") as f:
             code = f.read()
-        exec(code, globals())
+        exec(code, temp_namesp)
 
-    def save_action(self, *args, text):
-        return self.api(text)
+    def save_action(self, custom_api_path, temp_namesp, text):
+        return self.api(temp_namesp["custom_api"], text)
 
     def refresh_custom_api_list(self):
         self.custom_api_list = ['None']
@@ -60,4 +60,4 @@ class Custom(TTSProjet):
             gr.Info(i18n('Please select a valid custom API code file!'))
             raise Exception(i18n('Please select a valid custom API code file!'))
         kwargs = {'in_files': input_file, 'fps': fps, 'offset': offset, 'proj': "custom", 'max_workers': workers}
-        return (custom_api, None), kwargs  #
+        return (custom_api, dict()), kwargs  #

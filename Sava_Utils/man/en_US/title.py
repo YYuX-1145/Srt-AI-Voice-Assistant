ca11378315430aaf3375417f1a7c65e1e8ed19ea
YYuX-1145 <138500330+YYuX-1145@users.noreply.github.com>
2025-08-10 18:15:04 +0800

    Update version number to 4.6.3

--- Sava_Utils/man/en_US/title.py
+++ Sava_Utils/man/en_US/title.py
@@ -1,4 +1,4 @@
 title = r"""
-Version 4.6.2-2507<br>
+Version 4.6.3-2508<br>
 GitHub: [Check for updates manully](https://github.com/YYuX-1145/Srt-AI-Voice-Assistant/releases) | [Install Extensions](https://github.com/YYuX-1145/Srt-AI-Voice-Assistant/tree/main/Sava_Extensions)  
 """
